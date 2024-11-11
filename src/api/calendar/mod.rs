1b0235a73e458214024eb01d0c34043e76f939fc
Michal Piotrowski <practicalrs.com@gmail.com>
2024-11-11 12:49:11 +0100

    feat: Extract text from request (#3)

--- src/api/calendar/mod.rs
+++ src/api/calendar/mod.rs
@@ -1,8 +1,26 @@
 use crate::{app_error::AppError, context::Context};
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum::{
+    body::Body,
+    extract::{Request, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+use http_body_util::BodyExt;
 use std::sync::Arc;
 
 #[axum_macros::debug_handler]
-pub async fn action(State(_context): State<Arc<Context>>) -> Result<impl IntoResponse, AppError> {
+pub async fn action(
+    State(_context): State<Arc<Context>>,
+    request: Request<Body>,
+) -> Result<impl IntoResponse, AppError> {
+    let body = BodyExt::collect(request.into_body())
+        .await?
+        .to_bytes()
+        .to_vec();
+
+    let payload = String::from_utf8(body)?;
+
+    tracing::info!("{:?}", payload);
     Ok((StatusCode::OK, Json("{}")).into_response())
 }
--- src/app_error.rs
+++ src/app_error.rs
@@ -5,22 +5,34 @@ use axum::{
 };
 use serde::Serialize;
 use serde_json::json;
-use std::error::Error;
+use std::{error::Error, string::FromUtf8Error};
 use strum_macros::Display;
 use tracing::error;
 
 #[derive(Debug, Display)]
 pub enum AppError {
+    Axum(axum::Error),
     Generic(Box<dyn Error + Send + Sync>),
+    Utf8(FromUtf8Error),
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let (status, error_message) = match self {
+            AppError::Axum(error) => {
+                error!("Error: {:?}", error);
+
+                (StatusCode::INTERNAL_SERVER_ERROR, "Axum error.")
+            }
             AppError::Generic(error) => {
                 error!("Error: {:?}", error);
 
                 (StatusCode::INTERNAL_SERVER_ERROR, "Generic error.")
+            }
+            AppError::Utf8(error) => {
+                error!("Error: {:?}", error);
+
+                (StatusCode::INTERNAL_SERVER_ERROR, "Utf8 error.")
             }
         };
 
@@ -32,9 +44,21 @@ impl IntoResponse for AppError {
     }
 }
 
+impl From<axum::Error> for AppError {
+    fn from(inner: axum::Error) -> Self {
+        AppError::Axum(inner)
+    }
+}
+
 impl From<Box<dyn Error + Send + Sync>> for AppError {
     fn from(inner: Box<dyn Error + Send + Sync>) -> Self {
         AppError::Generic(inner)
+    }
+}
+
+impl From<FromUtf8Error> for AppError {
+    fn from(inner: FromUtf8Error) -> Self {
+        AppError::Utf8(inner)
     }
 }
 
