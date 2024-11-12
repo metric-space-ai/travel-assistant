0ce1f78d96e088f3e6175487317a818f847b5ec2
Michal Piotrowski <practicalrs.com@gmail.com>
2024-11-12 14:50:42 +0100

    feat: Implement logic for calendar (#6) p2

--- src/calendar_agent.rs
+++ src/calendar_agent.rs
@@ -25,17 +25,17 @@ pub async fn agent(context: Arc<Context>, payload: String) -> Result<String> {
     tracing::info!("calendars {:?}", calendars);
     for calendar in calendars {
         tracing::info!("calendar {:?}", calendar);
-        let new_event = Event::builder(calendar.url().clone())
-            //.description(Some("Sample event description".to_string()))
-            //.end("20241112T210000Z".to_string(), vec![])
-            //.location(Some("Sample event location".to_string()))
-            //.priority("1".to_string())
-            .start("20241112T190000Z".to_string(), vec![])
-            //.status("CONFIRMED".to_string())
-            .summary("Sample event summary".to_string())
-            .build();
-        tracing::info!("new_event = {:?}", new_event);
-        let new_event = minicaldav::save_event(agent.clone(), &credentials, new_event)?;
+        let event_post = EventPost {
+            description: Some("Sample event description".to_string()),
+            end: Some("20241112T210000Z".to_string()),
+            location: Some("Sample event location".to_string()),
+            priority: Some("1".to_string()),
+            start: "20241112T190000Z".to_string(),
+            status: Some("CONFIRMED".to_string()),
+            summary: "Sample event summary".to_string(),
+        };
+
+        let new_event = save_event(agent.clone(), &credentials, event_post, calendar.url().clone()).await?;
         tracing::info!("new_event = {:?}", new_event);
         let (events, errors) = minicaldav::get_events(agent.clone(), &credentials, &calendar)?;
         for event in events {
@@ -47,4 +47,22 @@ pub async fn agent(context: Arc<Context>, payload: String) -> Result<String> {
     }
 
     Ok(payload)
+}
+
+pub async fn save_event(agent: Agent, credentials: &Credentials, event_post: EventPost, url: Url) -> Result<Event> {
+    let new_event = Event::builder(url)
+    //.description(Some("Sample event description".to_string()))
+    //.end("20241112T210000Z".to_string(), vec![])
+    //.location(Some("Sample event location".to_string()))
+    //.priority("1".to_string())
+    .start("20241112T190000Z".to_string(), vec![])
+    //.status("CONFIRMED".to_string())
+    .summary("Sample event summary".to_string())
+    .build();
+
+    tracing::info!("new_event = {:?}", new_event);
+
+    let new_event = minicaldav::save_event(agent.clone(), credentials, new_event)?;
+
+    Ok(new_event)
 }
