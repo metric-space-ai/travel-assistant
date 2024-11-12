859b223d76341775ddc158bc1bb65bb2f61480c6
Michal Piotrowski <practicalrs.com@gmail.com>
2024-11-12 14:35:04 +0100

    feat: Implement logic for calendar (#6)

--- src/calendar_agent.rs
+++ src/calendar_agent.rs
@@ -4,6 +4,16 @@ use std::sync::Arc;
 use ureq::Agent;
 use url::Url;
 
+pub struct EventPost {
+    pub description: Option<String>,
+    pub end: Option<String>,
+    pub location: Option<String>,
+    pub priority: Option<String>,
+    pub start: String,
+    pub status: Option<String>,
+    pub summary: String,
+}
+
 pub async fn agent(context: Arc<Context>, payload: String) -> Result<String> {
     let agent = Agent::new();
     let url = Url::parse(&context.config.caldav_url)?;
@@ -15,18 +25,23 @@ pub async fn agent(context: Arc<Context>, payload: String) -> Result<String> {
     tracing::info!("calendars {:?}", calendars);
     for calendar in calendars {
         tracing::info!("calendar {:?}", calendar);
-        let new_event = Event::builder(calendar.url().clone())
-            .summary("Sample event summary".to_string())
-            .description(Some("Sample event description".to_string()))
-            .end("20241112T210000Z".to_string(), vec![])
-            .etag(Some("686897696a7c876b7e".to_string()))
-            .location(Some("Sample event location".to_string()))
-            .priority("1".to_string())
-            .start("20241112T190000Z".to_string(), vec![])
-            .status("CONFIRMED".to_string())
-            .build();
-        tracing::info!("new_event = {:?}", new_event);
-        let new_event = minicaldav::save_event(agent.clone(), &credentials, new_event)?;
+        let event_post = EventPost {
+            description: Some("Sample event description".to_string()),
+            end: Some("20241113T160000Z".to_string()),
+            location: Some("Sample event location".to_string()),
+            priority: Some("1".to_string()),
+            start: "20241113T150000Z".to_string(),
+            status: Some("CONFIRMED".to_string()),
+            summary: "Sample event summary 2".to_string(),
+        };
+
+        let new_event = save_event(
+            agent.clone(),
+            &credentials,
+            event_post,
+            calendar.url().clone(),
+        )
+        .await?;
         tracing::info!("new_event = {:?}", new_event);
         let (events, errors) = minicaldav::get_events(agent.clone(), &credentials, &calendar)?;
         for event in events {
@@ -38,4 +53,43 @@ pub async fn agent(context: Arc<Context>, payload: String) -> Result<String> {
     }
 
     Ok(payload)
+}
+
+pub async fn save_event(
+    agent: Agent,
+    credentials: &Credentials,
+    event_post: EventPost,
+    url: Url,
+) -> Result<Event> {
+    let event_builder = Event::builder(url)
+        .description(event_post.description)
+        .location(event_post.location)
+        .start(event_post.start, vec![])
+        .summary(event_post.summary);
+
+    let event_builder = if let Some(end) = event_post.end {
+        event_builder.end(end, vec![])
+    } else {
+        event_builder
+    };
+
+    let event_builder = if let Some(priority) = event_post.priority {
+        event_builder.priority(priority)
+    } else {
+        event_builder
+    };
+
+    let event_builder = if let Some(status) = event_post.status {
+        event_builder.status(status)
+    } else {
+        event_builder
+    };
+
+    let event = event_builder.build();
+
+    tracing::info!("event = {:?}", event);
+
+    let new_event = minicaldav::save_event(agent, credentials, event)?;
+
+    Ok(new_event)
 }
